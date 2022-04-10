4fc51ab474f751e1faea69a963a8b5d14439f4ff
Fabian Schneider <mail@fabschneider.com>
2022-04-10 18:31:23 +0200

    Fixes #11

--- Assets/WorldMod/Scripts/MultiDisplayManager.cs
+++ Assets/WorldMod/Scripts/MultiDisplayManager.cs
@@ -7,7 +7,10 @@ namespace Fab.WorldMod
         void Awake()
         {
 			if (Display.displays.Length > 1)
+			{
+				Debug.Log("Found multiple displays. Activating display 1");
 				Display.displays[1].Activate();
+			}
 		}
     }
 }
